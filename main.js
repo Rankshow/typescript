b814e8d6035eedc6c5eb6846168f659d704bce10
Rankshow <uwaomaobinna20@gmail.com>
2023-07-06 17:52:45 +0100

    variable declaration and data type structure

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "how are you and what is your mother name, her age range?!!";
+console.log(message);
+var isBegginer = true;
+var total = 0;
+var name = "micheal";
+var sentence = "My name is ".concat(name, ", and i am a beginner ").concat(isBegginer, " in typescript");
+console.log(sentence);
---
+++ main.ts
@@ -0,0 +1,10 @@
+export {}
+let message = "how are you and what is your mother name, her age range?!!";
+console.log(message);
+
+let isBegginer: Boolean = true;
+let total: number = 0;
+let name: string = "micheal";
+
+let sentence: string = `My name is ${name}, and i am a beginner ${isBegginer} in typescript`;
+console.log(sentence);
